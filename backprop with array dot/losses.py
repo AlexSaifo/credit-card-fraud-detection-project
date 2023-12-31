cdbbacbd776a81d179ad9012b9d6651b7e773eab
Moaaz TAL <moaaz.tal@gmail.com>
2023-12-31 15:29:36 +0300

    .

--- backprop with array dot/losses.py
+++ backprop with array dot/losses.py
@@ -1,11 +1,9 @@
 import numpy as np
 
 def mse(y_true, y_pred, w0=1, w1=1):
-    print ("mse: ",w0,w1)
     return np.mean( ((w1-w0)*y_true+w0) * np.power(y_true - y_pred, 2))
 
 def mse_prime(y_true, y_pred, w0=1, w1=1):
-    print ("prime mse: ",w0,w1)
     return ((w1-w0)*y_true+w0) * 2 * (y_pred - y_true) / np.size(y_true)
 
 def binary_cross_entropy(y_true, y_pred):
